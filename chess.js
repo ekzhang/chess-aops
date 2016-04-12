ec9a9dc0a2b7b2ebf2a69c9cfa4000502f3dc9e8
Eric Zhang <ekzhang1@gmail.com>
2016-04-11 20:51:26 -0500

    edited code to cause less issues in future

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-(function(ROOM_ID) {
+!function(ROOM_ID) {
   'use strict';
   var START_B = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
                  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
@@ -35,9 +35,91 @@
     if (q.length > 0) _doInput(q.shift());
   }, 1100);
 
+  var sendFEN = function(s) {
+    /* this is the hacky API call */
+    var x = 'http://www.gilith.com/chess/diagrams/?f=' + s.replace(/\//g, '%2F') + '&s=create';
+    var y = 'http://www.gilith.com/chess/diagrams/images/' + s.replace(/\//g, '_') + '.png';
+    $('<iframe src="' + x + '">').appendTo('body').load(function() {
+      $(this).remove();
+      w.doInput('[img]' + y + '[/img]');
+    });
+  }
+
+  var sendBoard = function() {
+    var fen = [];
+    for (var i = 0; i < 8; ++i) {
+      var c = [];
+      var t = 0;
+      for (var j = 0; j < 8; ++j) {
+        if (board[i][j]) {
+          if (t) {
+            c.push(t); 
+            t = 0;
+          }
+          c.push(side[i][j] ? board[i][j].toLowerCase() : board[i][j]);
+        }
+        else {
+          ++t;
+        }
+      }
+      if (t) c.push(t);
+      fen.push(c.join(''));
+    }
+    sendFEN(fen.join('/'));
+  }
+
+  var sendMod = function(s) {
+    w.doInput('[b]{@chess} ' + s + '[/b]');
+  }
+
+  var parseMove = function(m) {
+    /* this is a basic variant, no checking yet */
+    var a, b;
+    [a, b] = m;
+    try {
+      var x = function(s) { return s.charCodeAt(0) - 'a'.charCodeAt(0) };
+      var a1 = 8 - parseInt(a[1]);
+      var a2 = x(a[0]);
+      var b1 = 8 - parseInt(b[1]);
+      var b2 = x(b[0]);
+      board[b1][b2] = board[a1][a2];
+      side[b1][b2] = side[a1][a2];
+      board[a1][a2] = '';
+      side[a1][a2] = -1;
+    }
+    catch (err) {
+      console.error(err);
+      return false;
+    }
+    return true;
+  }
+
+  var nextTurn = function() {
+    turn = 1 - turn;
+    sendBoard();
+  }
+
+  var play = function(player1, player2) {
+    var t = Math.floor(Math.random() * 2);
+    white = [player1, player2][t];
+    black = [player2, player1][t];
+    turn = 0;
+    board = $.extend(true, [], START_B);
+    side = $.extend(true, [], START_S);
+    playing = true;
+    sendMod('The game has started. It is ' + white + '\'s turn.');
+    sendBoard();
+  }
+
+  var stop = function() {
+    playing = false;
+    sendMod('The game has ended.');
+  }
+
   var _onPluginMessage = Classroom.socket.onPluginMessage;
   Classroom.socket.onPluginMessage = function onPluginMessage(payload) {
-    if (payload.message && payload.message.startsWith('@chess')) {
+    if (payload['room-id'] == ROOM_ID && payload.message
+        && payload.message.startsWith('@chess')) {
       var x = payload.message.split(' ');
       if (playing) {
         if ((payload.speaker === white || payload.speaker === black)
@@ -64,84 +146,4 @@
     _onPluginMessage(payload);
   };
 
-  function sendFEN(s) {
-    var x = 'http://www.gilith.com/chess/diagrams/?f=' + s.replace(/\//g, '%2F') + '&s=create';
-    var y = 'http://www.gilith.com/chess/diagrams/images/' + s.replace(/\//g, '_') + '.png';
-    $('<iframe src="' + x + '">').appendTo('body').load(function() {
-      $(this).remove();
-      w.doInput('[img]' + y + '[/img]');
-    });
-  }
-
-  function sendBoard() {
-    var fen = [];
-    for (var i = 0; i < 8; ++i) {
-      var c = [];
-      var t = 0;
-      for (var j = 0; j < 8; ++j) {
-        if (board[i][j]) {
-          if (t) {
-            c.push(t); 
-            t = 0;
-          }
-          c.push(side[i][j] ? board[i][j].toLowerCase() : board[i][j]);
-        }
-        else {
-          ++t;
-        }
-      }
-      if (t) c.push(t);
-      fen.push(c.join(''));
-    }
-    sendFEN(fen.join('/'));
-  }
-
-  function sendMod(s) {
-    w.doInput('[b]{@chess} ' + s + '[/b]');
-  }
-
-  function parseMove(m) {
-    /* this is a basic variant, no checking yet */
-    var a, b;
-    [a, b] = m;
-    try {
-      var x = function(s) { return s.charCodeAt(0) - 'a'.charCodeAt(0) };
-      var a1 = 8 - parseInt(a[1]);
-      var a2 = x(a[0]);
-      var b1 = 8 - parseInt(b[1]);
-      var b2 = x(b[0]);
-      board[b1][b2] = board[a1][a2];
-      side[b1][b2] = side[a1][a2];
-      board[a1][a2] = '';
-      side[a1][a2] = -1;
-    }
-    catch (err) {
-      console.error(err);
-      return false;
-    }
-    return true;
-  }
-
-  function nextTurn() {
-    turn = 1 - turn;
-    sendBoard();
-  }
-
-  function play(player1, player2) {
-    var t = Math.floor(Math.random() * 2);
-    white = [player1, player2][t];
-    black = [player2, player1][t];
-    turn = 0;
-    board = $.extend(true, [], START_B);
-    side = $.extend(true, [], START_S);
-    playing = true;
-    sendMod('The game has started. It is ' + white + '\'s turn.');
-    sendBoard();
-  }
-
-  function stop() {
-    playing = false;
-    sendMod('The game has ended.');
-  }
-
-})(971);
+}(971);
