95683500c3ef6844de6db9396b178962df732ff9
stardark999 <stardark999@gmail.com>
2016-04-12 20:31:14 -0700

    main rules added (no checks yet)

--- chess.js
+++ chess.js
@@ -82,12 +82,20 @@
       var a2 = x(a[0]);
       var b1 = 8 - parseInt(b[1]);
       var b2 = x(b[0]);
-      board[b1][b2] = board[a1][a2];
-      side[b1][b2] = side[a1][a2];
-      board[a1][a2] = '';
-      side[a1][a2] = -1;
-    }
-    catch (err) {
+      var good = threaten_list(board, a1, a2);
+      var inList = false;
+      for (var i = 0; i < good.length; i++) {
+        if (good[i][0] === b1 && good[i][1] === b2) {
+          inList = true;
+        }
+      }
+      if (inList) {
+        board[b1][b2] = board[a1][a2];
+        side[b1][b2] = side[a1][a2];
+        board[a1][a2] = '';
+        side[a1][a2] = -1;
+      } else { w.doInput("oops invalid move"); }
+    } catch (err) {
       console.error(err);
       return false;
     }
@@ -114,6 +122,152 @@
   var stop = function() {
     playing = false;
     sendMod('The game has ended.');
+  };
+  
+  var counter = function(start = 1, end = 8) {
+    var c = start;
+    var toReturn = [];
+    while (c != end) {
+      // yield c; 
+      toReturn.push(c);
+      c++;
+    }
+    return toReturn;
+  };
+
+  var threaten_list = function(board, a, b) {
+    var ret = [];
+    var size = 8;
+    if (board[a][b] === 'P') {
+      if (side[a][b] === 0) {
+        if (board[a - 1][b] === '') {
+          ret.push([a - 1, b]);
+          if (a === size - 2 && board[a - 2][b] === '') {
+            ret.push([a - 2, b]);
+          }
+        }
+        for (i = b - 1; i <= b + 1; i += 2) { // i hate for-in loops
+          if (0 <= i && i < size && side[a - 1][i] === 1) {
+            ret.push([a - 1, i]);
+          }
+        }
+      } else {
+        if (board[a + 1][b] === '') {
+          ret.push([a + 1, b]);
+          if (a === 1 && board[a + 2][b] === '') {
+            ret.push([a + 2, b]);
+          }
+        }
+        for (i = b - 1; i <= b + 1; i += 2) { // for-in
+          if (0 <= i && i < size && side[a + 1][i] === 0) {
+            ret.push([a + 1, i]);
+          }
+        }
+      }
+    }
+    if (board[a][b] === 'K') {
+      for (var i = a - 1; i <= a + 1; i++) {
+        for (var j = b - 1; j <= b + 1; j++) {
+          if (i === a && j === b) {
+            continue;
+          }
+          if (0 <= i && i < size && 0 <= j && j < size
+                  && side[i][j] !== side[a][b]) {
+            ret.push([i, j]);
+          }
+        }
+      }
+    }
+    if (board[a][b] === 'N') {
+      var KNIGHT_MOVES = [[1, 2], [2, 1], [-1, 2], [-2, 1],
+            [1, -2], [2, -1], [-1, -2], [-2, -1]];
+      for (var i = 0; i < KNIGHT_MOVES.length; i++) {
+        var m = KNIGHT_MOVES[i];
+        if (0 <= a + m[0] && a + m[0] < size && 0 <= b + m[1] && b + m[1] < size) {
+          if (side[a][b] != side[a + m[0]][b + m[1]]) {
+            ret.push([a + m[0], b + m[1]]);
+          }
+        }
+      }
+    }
+    if (board[a][b] === 'R' || board[a][b] === 'Q') {
+      var c = counter();
+      for (var j = 0; j < c.length; j++) {
+        var i = c[j];
+        if (a + i >= size) {
+          break;
+        }
+        if (side[a + i][b] === side[a][b]) {
+          break;
+        }
+        ret.push([a + i][b]);
+        if (board[a + i][b] !== ' ') {
+          break;
+        }
+      }
+      for (var j = 0; j < c.length; j++) {
+        var i = c[j];
+        if (a - i < 0) {
+          break;
+        }
+        if (side[a - i][b] === side[a][b]) {
+          break;
+        }
+        ret.push([a - i][b]);
+        if (board[a - i][b] !== ' ') {
+          break;
+        }
+      }
+      for (var j = 0; j < c.length; j++) {
+        var i = c[j];
+        if (b + i > size) {
+          break;
+        }
+        if (side[a][b + i] === side[a][b]) {
+          break;
+        }
+        ret.push([a][b + i]);
+        if (board[a][b + i] !== ' ') {
+          break;
+        }
+      }
+      for (var j = 0; j < c.length; j++) {
+        var i = c[j];
+        if (b - i > size) {
+          break;
+        }
+        if (side[a][b - i] === side[a][b]) {
+          break;
+        }
+        ret.push([a][b - i]);
+        if (board[a][b - i] !== ' ') {
+          break;
+        }
+      }
+    }
+    if (board[a][b] === 'B' || board[a][b] === 'Q') {
+      var BISHOP_MOVES = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+      for (var j = 0; j < BISHOP_MOVES.length; j++) {
+        var m = BISHOP_MOVES[j];
+        var c = counter();
+        for (var k = 0; k < c.length; k++) {
+          var i = c[k];
+          var na = a + m[0] * i;
+          var nb = b + m[1] * i;
+          if (!(0 <= na && na < size && 0 <= nb && nb < size)) {
+            break;
+          }
+          if (side[na][nb] === side[a][b]) {
+            break;
+          }
+          ret.push([na, nb]);
+          if (board[na][nb] !== '') {
+            break;
+          }
+        }
+      }
+    }
+    return ret;
   };
 
   var _onPluginMessage = Classroom.socket.onPluginMessage;
